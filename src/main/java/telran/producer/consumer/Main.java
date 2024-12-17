3636c69863e5b18423ca5294659698bf523a7c52
Yuragranovsky <yuragranovsky@gmail.com>
2024-12-17 12:22:03 +0200

    HW #49 solution even-odd-messages

--- src/main/java/telran/producer/consumer/Main.java
+++ src/main/java/telran/producer/consumer/Main.java
@@ -5,15 +5,25 @@ public class Main {
     static final int N_RECEIVERS = 10;
 
     public static void main(String[] args) throws InterruptedException {
-        MessageBox messageBox = new SimpleMessageBox();
-        Sender sender = new Sender(N_MESSAGES, messageBox);
+        MessageBox messageBoxEven = new SimpleMessageBox();
+        MessageBox messageBoxOdd = new SimpleMessageBox();
+        Sender sender = new Sender(N_MESSAGES, messageBoxEven, messageBoxOdd);
+
         for (int i = 0; i < N_RECEIVERS; i++) {
-            new Receiver(messageBox).start();
+            Receiver receiver = new Receiver(null);
+            MessageBox messageBox = isReceiverEven(receiver) ? messageBoxEven : messageBoxOdd;
+            receiver.setMessageBox(messageBox);
+            receiver.start();
         }
         sender.start();
         sender.join();
-      
+
         Thread.sleep(100);
-       
+
+    }
+
+    private static boolean isReceiverEven(Receiver receiver) {
+        long id = receiver.getId();
+        return id % 2 == 0;
     }
 }
--- src/main/java/telran/producer/consumer/Sender.java
+++ src/main/java/telran/producer/consumer/Sender.java
@@ -2,16 +2,20 @@ package telran.producer.consumer;
 
 public class Sender extends Thread{
     private int nMessages;
-    private MessageBox messageBox;
-    public Sender(int nMessages, MessageBox messageBox) {
+    private MessageBox messageBoxEven;
+    private MessageBox messageBoxOdd;
+    public Sender(int nMessages, MessageBox messageBoxEven, MessageBox messageBoxOdd) {
         this.nMessages = nMessages;
-        this.messageBox = messageBox;
+        this.messageBoxEven = messageBoxEven;
+        this.messageBoxOdd = messageBoxOdd;
     }
     @Override
     public void run(){
         for(int i = 0; i < nMessages; i++) {
             try {
-                messageBox.put("Message" + (i +1));
+                int seqNum = i + 1;
+                MessageBox messageBox = seqNum % 2 == 0 ? messageBoxEven : messageBoxOdd;
+                messageBox.put("Message" + seqNum);
             } catch (InterruptedException e) {
                 
             }
